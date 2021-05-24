a0a55d665f2fb64320d0e34be595a6850f36bd5b
Pablo A <pablo.silva@ftdata.com.br>
2021-05-24 11:29:25 -0300

    Adicionados valores iniciais "n/a" aos atributos da classe Mensagem

--- src/main/java/br/com/fulltime/modelo/Mensagem.java
+++ src/main/java/br/com/fulltime/modelo/Mensagem.java
@@ -2,47 +2,63 @@ package br.com.fulltime.modelo;
 
 public class Mensagem {
 
-    private Aluno aluno;
-    private String disciplina;
-    private double primeiraNota;
-    private double segundaNota;
+    private Aluno aluno = new Aluno("n/a", "n/a");
+    private String disciplina = "n/a";
+    private String primeiraNota = "n/a";
+    private String segundaNota = "n/a";
 
-    public Mensagem(Aluno aluno, String disciplina, double primeiraNota, double segundaNota) {
-        this.aluno = aluno;
-        this.disciplina = disciplina;
-        this.primeiraNota = primeiraNota;
-        this.segundaNota = segundaNota;
+    public Mensagem() {
+//        this.aluno = aluno;
+//        this.disciplina = disciplina;
+//        this.primeiraNota = primeiraNota;
+//        this.segundaNota = segundaNota;
     }
 
-    public Aluno getAluno() {
-        return aluno;
+    public String getIdentificador() {
+        return this.aluno.getIdentificador();
     }
 
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
+    public String getNomeAluno() {
+        return this.aluno.getNomeAluno();
+    }
+
+    public void setAluno(String identificador, String nome) {
+        this.aluno.setNomeAluno(nome);
+        this.aluno.setIdentificador(identificador);
     }
 
     public String getDisciplina() {
-        return disciplina;
+        return this.disciplina;
     }
 
     public void setDisciplina(String disciplina) {
         this.disciplina = disciplina;
     }
 
-    public double getPrimeiraNota() {
-        return primeiraNota;
+    public String getPrimeiraNota() {
+        return this.primeiraNota;
     }
 
-    public void setPrimeiraNota(double primeiraNota) {
+    public void setPrimeiraNota(String primeiraNota) {
         this.primeiraNota = primeiraNota;
     }
 
-    public double getSegundaNota() {
-        return segundaNota;
+    public String getSegundaNota() {
+        return this.segundaNota;
     }
 
-    public void setSegundaNota(double segundaNota) {
+    public void setSegundaNota(String segundaNota) {
         this.segundaNota = segundaNota;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensagem: " +
+                "{Nome = " + getNomeAluno() +
+                ", Matéria = " + disciplina + '\'' +
+                ", Primeira Nota = " + primeiraNota + '\'' +
+                ", Segunda nota = " + segundaNota + '\'' +
+                ", ID = " + getIdentificador() + '\'' +
+                '}';
     }
 }
