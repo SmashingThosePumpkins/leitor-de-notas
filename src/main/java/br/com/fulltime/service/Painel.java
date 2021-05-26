e04e0b08da26615f3f634d0685d8311690b106cf
Pablo A <pablo.silva@ftdata.com.br>
2021-05-26 09:42:53 -0300

    Adicionado loop no painel inicial.

--- src/main/java/br/com/fulltime/service/Painel.java
+++ src/main/java/br/com/fulltime/service/Painel.java
@@ -13,59 +13,65 @@ public class Painel {
 
     public static ArrayList<Mensagem> painel() {
 
+        boolean rodando = true;
         ArrayList<Mensagem> lista = null;
-        String[] options = {"Checar todas as notas", "Adicionar uma nova nota", "Deletar uma nota"};
+        String[] options = {"Checar todas as notas", "Adicionar uma nova nota", "Deletar uma nota", "Sair"};
 
-        int input = JOptionPane.showOptionDialog(null, "O que deseja fazer?",
-                "Escolha uma opção.",
-                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        while (rodando) {
 
-        switch (input) {
-            case 0 -> {
-                var interpretador = new Interpretador();
-                try {
+            int input = JOptionPane.showOptionDialog(null, "O que deseja fazer?",
+                    "Escolha uma opção.",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
 
-                    lista = interpretador.getNotas();
-                    var displayText = new StringBuilder();
-                    lista.forEach((item) -> displayText.append(item));
+            switch (input) {
+                // Caso o usuário queira checar todas as notas
+                case 0 -> {
+                    var interpretador = new Interpretador();
+                    try {
 
-                    JOptionPane.showMessageDialog(null, displayText.toString());
+                        lista = interpretador.getNotas();
+                        var displayText = new StringBuilder();
+                        lista.forEach((item) -> displayText.append(item));
 
-                } catch (FileNotFoundException e) {
-                    JOptionPane.showMessageDialog(null, "Arquivo não encontrado");
-                    e.printStackTrace();
-                }
-            }
+                        JOptionPane.showMessageDialog(null, displayText.toString());
 
-            case 1 -> {
-                var aluno = new Aluno("n/a", "n/a");
-                var mensagem = new Mensagem();
-
-                try {
-                ManuseamentoArquivo.imprimirMensagem(aluno, mensagem);
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    } catch (FileNotFoundException e) {
+                        JOptionPane.showMessageDialog(null, "Arquivo não encontrado");
+                        e.printStackTrace();
+                    }
                 }
 
-                JOptionPane.showMessageDialog(null, "Nova nota cadastrada com sucesso.");
+                // Caso o usuário queira adicionar uma nova nota
+                case 1 -> {
+                    var aluno = new Aluno("n/a", "n/a");
+                    var mensagem = new Mensagem();
 
-            }
+                    try {
+                        ManuseamentoArquivo.imprimirMensagem(aluno, mensagem);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
-            case 2 -> {
-                var inputIdentificador = JOptionPane.showInputDialog(null, "Digite o identificador (número de seis dígitos).");
-                var inputDisciplina = JOptionPane.showInputDialog(null, "Digite a disciplina.");
+                    JOptionPane.showMessageDialog(null, "Nova nota cadastrada com sucesso.");
 
-                try {
-                    ManuseamentoArquivo.limparLinha(Long.parseLong(inputIdentificador), inputDisciplina);
-                } catch (IllegalFormatException e) {
-                    JOptionPane.showMessageDialog(null, "Não foi digitado um número. Tente novamente.");
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
-            }
 
-            default -> {
+                // Caso o usuário queira deletar uma nota
+                case 2 -> {
+                    var inputIdentificador = JOptionPane.showInputDialog(null, "Digite o identificador do aluno (número de seis dígitos).");
+                    var inputDisciplina = JOptionPane.showInputDialog(null, "Digite a disciplina.");
+
+                    try {
+                        ManuseamentoArquivo.limparLinha(Long.parseLong(inputIdentificador), inputDisciplina);
+                    } catch (IllegalFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Não foi digitado um número. Tente novamente.");
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                default -> rodando = false;
             }
         }
 
