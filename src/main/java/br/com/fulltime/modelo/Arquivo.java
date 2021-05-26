837c739410198ba85c079974e5eb3bdfa3b0c31c
Pablo A <pablo.silva@ftdata.com.br>
2021-05-26 08:50:17 -0300

    Pequena refatoração no modelo Arquivo

--- src/main/java/br/com/fulltime/modelo/Arquivo.java
+++ src/main/java/br/com/fulltime/modelo/Arquivo.java
@@ -2,7 +2,7 @@ package br.com.fulltime.modelo;
 
 public class Arquivo {
 
-    public static final String NOTAS_ALUNOS = "src/main/resources/notas_alunos.txt";
+    public static final String ARQUIVO_NOTAS = "src/main/resources/notas_alunos.txt";
     public static final int ARRAY_IDENTIFICADOR_ALUNO = 0;
     public static final int ARRAY_NOME_ALUNO = 1;
     public static final int ARRAY_DISCIPLINA = 2;
